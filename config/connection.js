5956afabf3e664e7332de468312a9e835a013596
Stephen Carlin <scarlinj@gmail.com>
2022-12-31 13:47:22 -0800

    connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -6,10 +6,15 @@ require('dotenv').config();
 // create connection to our database, pass in your MySQL information for username and password
 // use process.env.DB_NAME as local environment variable to hide sensitive data when pushed to Herooku
 // Sequelize prefers to work with mysql2 library instead of mysql
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+// use JawsDB to set up remote database
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
 });
+}
 
 module.exports = sequelize;
