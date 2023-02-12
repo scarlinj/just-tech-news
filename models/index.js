27941f85ed6fb149a0610093e220545366929da5
Stephen Carlin <scarlinj@gmail.com>
2023-02-11 20:31:11 -0800

    added Comment model and associations.  Server starts successfully.

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 const User = require('./User');
 const Post = require('./Post');
 const Vote = require('./Vote');
+const Comment = require('./Comment');
 
 // create associations
 // With the below two .belongsToMany() methods in place, we're allowing both the User and Post models to query each other's information in the context of a vote. 
@@ -43,9 +44,26 @@ Post.hasMany(Vote, {
   foreignKey: 'post_id'
 });
 
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id'
+});
+
 // export the user model as an object
 module.exports = {
   User,
   Post,
-  Vote
+  Vote,
+  Comment
 };
