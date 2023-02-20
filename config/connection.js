dc492d5b2cd4f10ba8dbed62bb003972bf88f4ea
Stephen Carlin <scarlinj@gmail.com>
2023-02-20 15:29:20 -0800

    try again to connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -3,11 +3,11 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-// use tge const Sequelize you imported above in the database connection below
+// use the const Sequelize you imported above in the database connection below
 let sequelize;
 // create connection to our database, pass in your MySQL information for username and password
 // use process.env.DB_NAME as local environment variable to hide sensitive data when pushed to Heroku
-// Sequelize prefers to work with mysql2 library instead of mysql
+// Sequelize prefers to work with mysql2 library instead of mysql - not working 2/20/2023.  Use mysql library
 // use JawsDB to set up remote database below
 if (process.env.JAWSDB_URL) {
     sequelize = new Sequelize(process.env.JAWSDB_URL);
