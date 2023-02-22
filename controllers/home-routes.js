7d517aa874deb174c13a6f99f211fbfe7d261e16
Stephen Carlin <scarlinj@gmail.com>
2023-02-21 21:56:06 -0800

    created homepage

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,8 +1,45 @@
 const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
 
-router.get('/', (req, res) => {
 // render a homepage, since we don't have the HTML
-  res.render('homepage');
-});
+router.get('/', (req, res) => {
+    Post.findAll({
+      attributes: [
+        'id',
+        'post_url',
+        'title',
+        'created_at',
+        [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+      ],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+      .then(dbPostData => {
+        // entire array of posts to be in the template
+        // This will loop over and map each Sequelize object into a serialized version of itself, saving the results in a new posts array, to plug into template
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+        // pass a single post object into the homepage template
+        console.log(dbPostData[0]);
+        // render the posts array in the homepage template
+        res.render('homepage', { posts });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
 
 module.exports = router;
