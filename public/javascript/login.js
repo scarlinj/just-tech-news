51302b8b8fcf0ba38cc6e1c8c13077e3b7ab90fb
Stephen Carlin <scarlinj@gmail.com>
2023-03-26 23:32:29 -0700

    edit order for queries on login.js

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -19,7 +19,7 @@ async function loginFormHandler(event) {
         document.location.replace('/dashboard');
     } else {
         alert(response.statusText);
-        document.location.replace('/');
+        document.location.replace('/login');
         console.log(response.statusText);
         console.log('didnt load.');
     }
